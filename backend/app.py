37eafe63ab9941f52c2206b5ddd3fda0d34ee674
MiliMade <milimadeemail@gmail.com>
2026-05-11 20:03:59 -0400

    build: Set up and connected database to app

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import os
 
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+
+
+#create the app
 app = Flask(__name__)
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+
+#configure postgres database
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost/medicine-tracker"
+
+#initialize app with extension
+db.init_app(app)
+
+
 
 @app.route("/")
 def hello_world():
