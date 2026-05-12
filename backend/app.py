ed7bfb7183cc36f780dc611247feb821b9180fdd
MiliMade <milimadeemail@gmail.com>
2026-05-12 14:28:27 -0400

    build: added user model

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 import os
 
 DB_USER = os.environ["DB_USER"]
@@ -21,6 +22,16 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{DB_USER}:{DB_PASSWORD}@l
 
 #initialize app with extension
 db.init_app(app)
+
+
+#User model
+class User(db.Model):
+  id:Mapped[int] = mapped_column(primary_key=True)
+  username: Mapped[str] 
+  email:Mapped[str]
+  password:Mapped[str]
+  medications:Mapped[list["Medications"]] = relationship(backref="user")
+
 
 
 
